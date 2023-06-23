2c1b402c709fdc9b12380af87d0338786c0c34c8
sosuisen <hidekazu.kubota@gmail.com>
2023-06-23 23:14:50 +0900

    fix: comment

--- src/main/java/com/example/auth/AuthenticationMechanismConfig.java
+++ src/main/java/com/example/auth/AuthenticationMechanismConfig.java
@@ -4,6 +4,17 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.security.enterprise.authentication.mechanism.http.CustomFormAuthenticationMechanismDefinition;
 import jakarta.security.enterprise.authentication.mechanism.http.LoginToContinue;
 
+/*
+ * 要認証のパスへのアクセスは、
+ * loginPageに指定されたパスへのアクセスへ読み変えられます。
+ * つまり下の場合 @GET @Path("login")のメソッドが呼ばれます。
+ * 
+ * この呼び出しは、useForwardToLoginがfalseのときはredirectで、
+ * trueのときはforwardで行われます。デフォルトはtrue。
+ * いずれも@GET @Path("login")のメソッドが呼ばれますが、
+ * forwardの場合、URLは元のパス（例えば/msg/list）のままで
+ * redirectの場合、URLは/msg/login に変わります。
+ */
 @CustomFormAuthenticationMechanismDefinition(
     loginToContinue = @LoginToContinue(
         loginPage="/msg/login",
