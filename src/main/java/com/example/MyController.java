e168a2cf2b83a0e5ac1e0106cbeecc8fd90fbb34
sosuisen <hidekazu.kubota@gmail.com>
2023-06-25 03:42:42 +0900

    fix: use FormAuthenticationMechanismDefinition

--- src/main/java/com/example/MyController.java
+++ src/main/java/com/example/MyController.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import java.net.URI;
-import java.util.List;
 
 import com.example.auth.IdentityStoreConfig;
 import com.example.model.message.MessageDTO;
@@ -15,16 +14,10 @@ import jakarta.inject.Inject;
 import jakarta.mvc.Controller;
 import jakarta.mvc.Models;
 import jakarta.mvc.MvcContext;
-import jakarta.security.enterprise.AuthenticationStatus;
 import jakarta.security.enterprise.SecurityContext;
-import jakarta.security.enterprise.authentication.mechanism.http.AuthenticationParameters;
-import jakarta.security.enterprise.credential.Credential;
-import jakarta.security.enterprise.credential.Password;
-import jakarta.security.enterprise.credential.UsernamePasswordCredential;
 import jakarta.security.enterprise.identitystore.Pbkdf2PasswordHash;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.ws.rs.BeanParam;
 import jakarta.ws.rs.ForbiddenException;
 import jakarta.ws.rs.FormParam;
@@ -54,10 +47,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(force = true)
 @Path("/")
 public class MyController {
-	private static String basePath; 
-	
 	private final Models models;
-	
+
 	private final MessagesDAO messagesDAO;
 
 	private final UsersDAO usersDAO;
@@ -75,8 +66,6 @@ public class MyController {
 		this.passwordHash = passwordHash;
 		passwordHash.initialize(IdentityStoreConfig.HASH_PARAMS);
 		this.securityContext = securityContext;
-		// staticクラスForbiddenExceptionMapperで用いるため、static変数へ代入しておく
-		basePath = mvcContext.getBasePath();
 	}
 
 	@GET
@@ -87,54 +76,8 @@ public class MyController {
 	@GET
 	@Path("login")
 	public String getLogin(@Context HttpServletRequest request,
-			@QueryParam("messages") final String messages,
-			@QueryParam("errors") final String errors) {
-		if (messages != null) {
-			models.put("messages", List.of(messages.split(",")));
-		}
-		if (errors != null) {
-			models.put("errors", List.of(errors.split(",")));
-		}
-		try {
-			request.logout();
-		} catch (ServletException e) {
-			e.printStackTrace();
-		}
-		return "login.jsp";
-	}
-
-	@POST
-	@Path("login")
-	public String postLogin(@BeanParam UserDTO user,
-			@Context HttpServletRequest request,
-			@Context HttpServletResponse response)
-			throws Exception {
-		Credential credential = new UsernamePasswordCredential(user.getName(), new Password(user.getPassword()));
-		AuthenticationStatus status = securityContext.authenticate(
-				request,
-				response,
-				AuthenticationParameters.withParams()
-						.newAuthentication(true)
-						.credential(credential));
-
-		/**
-		 * NOT_DONE
-		 *  認証メカニズムが呼び出されましたが、認証しないことにしました。
-		 * SEND_CONTINUE
-		 *  認証メカニズムが呼び出され、発信者とのマルチステップ認証ダイアログが開始されました（たとえば、発信者がログインページにリダイレクトされました）。
-		 * SEND_FAILURE
-		 *  認証メカニズムが呼び出されましたが、呼び出し元が正常に認証されなかったため、呼び出し元のプリンシパルは使用できません。
-		 * SUCCESS
-		 *  認証メカニズムが呼び出され、呼び出し元が正常に認証されました。
-		 */
-		switch (status) {
-			case SEND_CONTINUE, NOT_DONE -> {
-			}
-			case SEND_FAILURE -> models.put("errors", List.of("invalid_user"));
-			case SUCCESS -> {
-				return "redirect:list";
-			}
-		}
+			@QueryParam("error") final String error) {
+		models.put("error", error);
 		return "login.jsp";
 	}
 
@@ -142,18 +85,24 @@ public class MyController {
 	@Path("logout")
 	public String getLogout(@Context HttpServletRequest request) {
 		try {
-			request.logout();
+			request.logout(); // ログアウトする
+			request.getSession().invalidate(); // セッションを無効化する
 		} catch (ServletException e) {
 			e.printStackTrace();
 		}
-		return "redirect:login?messages=logout";
+		return "redirect:/";
 	}
 
 	@GET
 	@Path("list")
 	@RolesAllowed({ "USER", "ADMIN" })
-	public String getMessage() {
-		models.put("isAdmin", securityContext.isCallerInRole("ADMIN"));
+	public String getMessage(@Context HttpServletRequest req) {
+		// adminでログインしたときsecurityContext.isCallerInRole("ADMIN")が成功するべきですが、
+		// ログイン直後の最初の1回が失敗するバグがあります（2023/6/25現在）。
+		// models.put("isAdmin", securityContext.isCallerInRole("ADMIN"));
+		// 
+		// 回避策としてHttpServletRequestのisUserInRoleを用います。		
+		models.put("isAdmin", req.isUserInRole("ADMIN"));
 		models.put("name", securityContext.getCallerPrincipal().getName());
 		messagesDAO.getAll();
 		return "list.jsp";
@@ -224,15 +173,28 @@ public class MyController {
 	}
 
 	/*
-	 * 権限がないページへのアクセスは 404 Forbidden になるため、
+	 * 権限がないページへのアクセスは 403 Forbidden になるため、
 	 * 対応するExceptionMapperを追加
 	 */
 	@Provider
 	public static class ForbiddenExceptionMapper implements ExceptionMapper<ForbiddenException> {
+		private HttpServletRequest request;
+
+		@Inject
+		public ForbiddenExceptionMapper(@Context HttpServletRequest request) {
+			this.request = request;
+		}
+
 		@Override
 		public Response toResponse(ForbiddenException exception) {
+			try {
+				request.logout();
+				request.getSession().invalidate();
+			} catch (ServletException e) {
+				e.printStackTrace();
+			}
 			// ExceptionMapper内でのリダイレクト実行は Response.seeOther()
-			return Response.seeOther(URI.create(MyController.basePath + "/login?errors=forbidden")).build();
+			return Response.seeOther(URI.create(request.getRequestURL().toString() + "?error=forbidden")).build();
 		}
 	}
 }
--- src/main/java/com/example/auth/AuthenticationMechanismConfig.java
+++ src/main/java/com/example/auth/AuthenticationMechanismConfig.java
@@ -1,7 +1,7 @@
 package com.example.auth;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.security.enterprise.authentication.mechanism.http.CustomFormAuthenticationMechanismDefinition;
+import jakarta.security.enterprise.authentication.mechanism.http.FormAuthenticationMechanismDefinition;
 import jakarta.security.enterprise.authentication.mechanism.http.LoginToContinue;
 
 /*
@@ -13,13 +13,13 @@ import jakarta.security.enterprise.authentication.mechanism.http.LoginToContinue
  * trueのときはforwardで行われます。デフォルトはtrue。
  * いずれも@GET @Path("login")のメソッドが呼ばれますが、
  * forwardの場合、URLは元のパス（例えば/msg/list）のままで
- * redirectの場合、URLは/msg/login に変わります。
+ * redirectの場合、URLは/msg/login に変わります。通常はtrueで良いです。
  */
-@CustomFormAuthenticationMechanismDefinition(
+@FormAuthenticationMechanismDefinition(
     loginToContinue = @LoginToContinue(
         loginPage="/msg/login",
-        errorPage="/msg/login",
-        useForwardToLogin = false
+        errorPage="/msg/login?error=invalid_user",
+        useForwardToLogin = true
     )
 )
 @ApplicationScoped
